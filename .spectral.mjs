38c9a4dfe84c0222ca4f37c6c8999f9763220526
Yuki Hayashi <mmmr0628@gmail.com>
2024-02-21 18:10:46 +0900

    Modified 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIzNzM5Ng/spectral.js?branch_slug=main&token=d6dfd50d-62bb-443a-91d1-a6684ef898f8";
+export default { extends: ruleset };
